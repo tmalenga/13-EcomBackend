5abbafe1e4586d9f2775132f0ec09e09e995f4e4
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-03-17 22:18:03 -0300

    working code and readme

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
   const tag_data = await Tag.findAll({
     include:[{model: Product, attributes:['id', 'product_name', 'price', 'stock', 'category_id'],}]
   })
-  res.status(500).json(tag_data);
+  res.status(200).json(tag_data);
 
 });
 
@@ -19,14 +19,14 @@ router.get('/:id', async (req, res) => {
   const tag_data = await Tag.findByPk(req.params.id,{
     include:[{model: Product, attributes:['id', 'product_name', 'price', 'stock', 'category_id']}]
   })
-  res.status(500).json(tag_data);
+  res.status(200).json(tag_data);
 
 });
 
 router.post('/', async (req, res) => {
   // create a new tag
-  const tag_data = await Tag.create({tag_name: req.body.tag_name});
-  res.status(500).json(tag_data);
+  const tag_data = await Tag.create(req.body);
+  res.status(200).json(tag_data);
 
 });
 
@@ -35,14 +35,14 @@ router.put('/:id', async (req, res) => {
   const tag_data = await Tag.update(req.body,{
     where: {id: req.params.id}
   })
-  res.status(500).json(tag_data);
+  res.status(200).json(tag_data);
 
 });
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   const tag_data = await Tag.destroy({ where: {id: req.params.id}});
-  res.status(500).json(tag_data);
+  res.status(200).json(tag_data);
 });
 
 module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # E-commerce Back End Starter Code
+## Description
+
+Application is an e-commerce backend that uses node js, sequilize and sql to create, read, update and delete from the database.
+
+## Installation
+
+N/A
+
+## Usage
+
+- This is an application that will allow the user to :
+    - Create, 
+    - Read,
+    - Update and
+    - Delete data from an ecommerce database.
+- Based on the response provided by the user the application will populate the desired action, see walkthrough video below.
+- Download walkthrough video ![here](./EcomBackendWT.webm)
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to LICENSE in the repo
